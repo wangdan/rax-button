ec2997e3ee13f88aae2ee9969f974bb5839ccf77
亚城 <narutosun2@163.com>
2019-01-07 17:29:42 +0800

    feat: use index.css

--- src/index.js
+++ src/index.js
@@ -2,49 +2,35 @@ import {Component, createElement} from 'rax';
 import {isWeex} from 'universal-env';
 import Text from 'rax-text';
 import Touchable from 'rax-touchable';
+import './index.css';
 
 class Button extends Component {
   static propTypes = {};
 
   render() {
     const props = this.props;
-    const buttonStyles = [styles.button, props.style];
-    const textStyles = [styles.text];
+    const textStyles = {};
+    let buttonClassName = 'text';
 
     if (props.color) {
-      textStyles.push({color: props.color});
+      textStyles.color = props.color;
     }
 
     if (props.disabled) {
-      buttonStyles.push(styles.buttonDisabled);
-      textStyles.push(styles.textDisabled);
+      buttonClassName += ' textDisabled';
     }
 
     let content = props.children || props.title;
     if (typeof content === 'string') {
-      content = <Text style={textStyles}>{content}</Text>;
+      content = <Text className={buttonClassName} style={textStyles}>{content}</Text>;
     }
 
     return (
-      <Touchable role="button" {...props} style={buttonStyles}>
+      <Touchable role="button" {...props}>
         {content}
       </Touchable>
     );
   }
 }
-
-const styles = {
-  button: {},
-  text: {
-    color: '#0C42FD',
-    textAlign: 'center',
-    padding: 16,
-    fontSize: 36,
-  },
-  buttonDisabled: {},
-  textDisabled: {
-    color: '#cdcdcd',
-  },
-};
 
 export default Button;
